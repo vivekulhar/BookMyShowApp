be4943462e98c760005e8272ca47dafc1ffefc84
Vivek Kulhar <vivek.kulhar23@gmail.com>
2024-01-26 15:02:30 +0530

    Home Login and Register Page

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./stylesheets/alignments.css";
+import "./stylesheets/sizes.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/custom.css";
+import "./stylesheets/theme.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

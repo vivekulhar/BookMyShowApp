e3102c42ca078c84af1e475be06531fefb6d2d5d
Vivek Kulhar <vivek.kulhar23@gmail.com>
2024-01-27 18:25:24 +0530

    UI of Admin

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import PageTitle from "../../components/PageTitle";
+import { Tabs } from "antd";
+import MoviesList from "./MoviesList";
+// import TheatresList from "./TheatresList";
+// import UpcomingList from "./UpcomingList";
 
-const Admin = () => {
-  return <div>index</div>;
-};
+function Admin() {
+  return (
+    <div>
+      <PageTitle title="Admin" />
+
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Movies" key="1">
+          <MoviesList/>
+        </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Theatres" key="2">
+          <h1>Theatre Table</h1>
+        </Tabs.TabPane>
+        {/* <Tabs.TabPane tab="Upcoming Movies" key="3">
+          <UpcomingList />
+        </Tabs.TabPane> */}
+      </Tabs>
+    </div>
+  );
+}
 
 export default Admin;

a28a1b64b6acf237c9943a90e834553b28a0ca52
Vivek Kulhar <vivek.kulhar23@gmail.com>
2024-02-10 12:39:15 +0530

    require to import again

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-require("./index.css");
-const App = require("./App");
-const reportWebVitals = require("./reportWebVitals");
-const store = require("./redux/store").default;
-const Provider = require("react-redux").Provider;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

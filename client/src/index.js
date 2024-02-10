38c1300714c30238c34231134c4a5bb8e1913a76
Vivek Kulhar <vivek.kulhar23@gmail.com>
2024-02-10 12:44:46 +0530

    .js extension and removed type module

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import reportWebVitals from "./reportWebVitals";
-import store from "./redux/store";
+import reportWebVitals from "./reportWebVitals.js";
+import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
